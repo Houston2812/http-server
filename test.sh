24cc23e16606681a45c60ccf7a61eb2502de2b7f
Huseyn Gambarov <qambarov2812@gmail.com>
2024-03-05 23:13:21 -0500

    Multi client test

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for (( i=1 ; i<=105 ; i++ )); 
 do
-   python3 client.py 127.0.0.1 /www/test_visual/index.html GET &
+   python3 client.py 127.0.0.1 /www/test_visual/index.html GET -m 3 &
    sleep 0.5
 done
